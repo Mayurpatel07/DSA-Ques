a165d2c0067d30b8e51b7c9fc2beba9f502fdd36
Patel Mayur Govind <143216324+Mayurpatel07@users.noreply.github.com>
2026-09-26 18:53:43 +0530

    Time: 0 ms (100%), Space: 20.6 MB (19.73%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,10 +1,16 @@
 class Solution:
     def missingNumber(self, nums: list[int]) -> int:
-        tsum = 0
-        sums = 0 
-        for i in range(1,len(nums)+1):
-            tsum +=i
-        for i in range(len(nums)):
-            sums += nums[i]
-        missing_num = tsum-sums
+        # tsum = 0
+        # sums = 0 
+        # for i in range(1,len(nums)+1):
+        #     tsum +=i
+        # for i in range(len(nums)):
+        #     sums += nums[i]
+        # missing_num = tsum-sums
+        # return missing_num    
+        tsum = 0 
+        n = len(nums)
+        tsum = n*(n+1)//2
+        sums = sum(nums)
+        missing_num = tsum - sums
         return missing_num    
